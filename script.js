89e97d7fe2e0a7d5ff1f77f86dc7ee67b2955e52
Mohammd Mehdi Rahimi <poryrahimi@gmail.com>
2024-12-18 12:57:23 +0330

    main page is done(with responsive)

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-document.documentElement.setAttribute("color-scheme", "light");
-const collapseMenu = document.getElementById("collapse-menu");
-collapseMenu.addEventListener("click", () => {
-  collapseMenu.classList.add("d-none");
-  const navbar2 = document.getElementById("navbar2");
-  navbar2.classList.remove("d-none");
-  navbar2.classList.add("d-flex");
+const colseButton = document.getElementById("close-btn");
+const navbar = document.getElementById("navbar");
+colseButton.addEventListener("click", () => {
+  if (navbar.classList.contains("open")) {
+    navbar.style.transform = "translateX(200%)";
+    navbar.classList.remove("open");
+    colseButton.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    navbar.style.transform = "translateX(0)";
+    navbar.classList.add("open");
+    colseButton.innerHTML = `x`;
+  }
 });
